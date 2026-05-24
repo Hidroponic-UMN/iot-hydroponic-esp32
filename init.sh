fe30a3f9027b75330582ad5634a82d2870cb90dc
ddavviidbunbun <david.bunyamin.99@gmail.com>
2026-05-24 11:13:45 +0700

    Update libraries

---
+++ init.sh
@@ -0,0 +1,11 @@
+# clone all this repo either by git clone or manually
+
+git clone git@github.com:milesburton/Arduino-Temperature-Control-Library.git
+
+git clone git@github.com:bblanchon/ArduinoJson.git
+
+git clone git@github.com:claws/BH1750.git
+
+git clone git@github.com:PaulStoffregen/OneWire.git
+
+git clone git@github.com:knolleary/pubsubclient.git
